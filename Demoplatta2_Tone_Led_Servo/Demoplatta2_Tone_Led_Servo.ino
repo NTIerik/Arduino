2fdc97b2df555a62f25b3fa5dc2aa7ef8ffc2355
NTI-Erik <12120727+NTIerik@users.noreply.github.com>
2022-02-05 10:41:52 +0100

    Update Demoplatta2_Tone_Led_Servo.ino

--- Demoplatta2_Tone_Led_Servo/Demoplatta2_Tone_Led_Servo.ino
+++ Demoplatta2_Tone_Led_Servo/Demoplatta2_Tone_Led_Servo.ino
@@ -239,21 +239,20 @@ delay(1000);
 // PLAY
 
 // iterate over the notes of the melody:
-for (int thisNote = 0; thisNote < 98; thisNote++) {
+  for (int thisNote = 0; thisNote < 98; thisNote++) {
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(8, melody[thisNote],noteDuration);
 
-// to calculate the note duration, take one second
-// divided by the note type.
-//e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-int noteDuration = 1000/noteDurations[thisNote];
-tone(8, melody[thisNote],noteDuration);
-
-// to distinguish the notes, set a minimum time between them.
-// the note's duration + 30% seems to work well:
-int pauseBetweenNotes = noteDuration * 1.30;
-delay(pauseBetweenNotes);
-// stop the tone playing:
-noTone(8);
-}
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
 }
 
 void loop() {
